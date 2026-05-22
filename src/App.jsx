10439845f23e6e91b7d6d6a44dcee267befe24c9
Deepak-Panchal-1003 <deepanchal1003@gmail.com>
2026-05-22 11:07:54 +0530

    Readme CHange

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
+    <div className='w-screen flex  h-screen items-center'>
+      <h1 className='text-blue-500 hover:text-red-500'>App</h1>
       <h3>Hello</h3>
-      <h5>Hello world my name is deepak panchal</h5>
+      <h5>Hello world my name is deepak panchal &apos;</h5>
       </div>
   )
 }
